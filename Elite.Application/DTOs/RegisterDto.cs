4c145e175524fc545530d183a7cdb9e88ed17f22
Precious-Adeoye <contact.preciousadeoye@gmail.com>
2025-06-27 14:24:37 +0100

    Updated model state

--- Elite.Application/DTOs/RegisterDto.cs
+++ Elite.Application/DTOs/RegisterDto.cs
@@ -1,13 +1,32 @@
-﻿namespace Elite.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elite.Application.DTOs
 {
     public class RegisterDto
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
         public string country { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 6 characters long")]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassWord { get; set; }
     }
 }
--- Elite.Application/DTOs/RequestOtpDto.cs
+++ Elite.Application/DTOs/RequestOtpDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Elite.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elite.DTOs
 {
     public class RequestOtpDto
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
     }
 }
